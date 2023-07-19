76093777eec9fdd72871cd443836ad56fd2a814f
luisholman <88738669+luisholman@users.noreply.github.com>
2023-07-19 13:19:57 -0500

    commit

--- js/jquery.isotope.js
+++ js/jquery.isotope.js
@@ -1,16 +1,4 @@
-/**
- * Isotope v1.5.25
- * An exquisite jQuery plugin for magical layouts
- * http://isotope.metafizzy.co
- *
- * Commercial use requires one-time license fee
- * http://metafizzy.co/#licenses
- *
- * Copyright 2012 David DeSandro / Metafizzy
- */
 
-/*jshint asi: true, browser: true, curly: true, eqeqeq: true, forin: false, immed: false, newcap: true, noempty: true, strict: true, undef: true */
-/*global jQuery: false */
 
 (function( window, $, undefined ){
 
